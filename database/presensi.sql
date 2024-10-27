56fedbc146d2c94b3c841b1fffce9539dbb8aabb
Shyy23 <syahrulhidayatulloh14@gmail.com>
2024-10-28 05:23:37 +0700

    databaseUP

--- database/presensi.sql
+++ database/presensi.sql
@@ -1,177 +1,510 @@
--- Membuat nama database
--- CREATE DATABASE presensi;
-
--- USE presensi;
-
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 27, 2024 at 11:21 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
--- USE testing;
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Table structure for table `siswa`
-CREATE TABLE siswa (
-    `id_siswa` INT PRIMARY KEY AUTO_INCREMENT,
-    `nisn` VARCHAR(10) NOT NULL UNIQUE,
-    `nama` VARCHAR(100) NOT NULL,
-    `jenis_kelamin` ENUM('L', 'P') NOT NULL,
-    `alamat` TEXT NOT NULL,
-    `id_kelas` INT(5) NOT NULL,
-    `no_telepon` varchar(20) NOT NULL,
-    `password` VARCHAR(255) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Database: `presensi_syahrull`
+--
 
--- Dumping data for table `siswa`
-INSERT INTO siswa (`id_siswa`, `nisn`, `nama`, `jenis_kelamin`, `alamat`, `id_kelas`, `no_telepon`, `password`) VALUES
-(33, '00000', 'Syahrul Hidayatulloh', 'L', '-', '11', '-', 'abcdefghij');
+-- --------------------------------------------------------
 
+--
 -- Table structure for table `absen`
-CREATE TABLE absen (
-    `id_absen` INT PRIMARY KEY AUTO_INCREMENT,
-    `id_siswa` INT NOT NULL,
-    `id_jadwal` INT NOT NULL,
-    `waktu` TIME DEFAULT CURRENT_TIMESTAMP,
-    `tanggal` DATE DEFAULT CURRENT_TIMESTAMP,
-    `keterangan` ENUM('H', 'S', 'I', 'A') NOT NULL,
-    CONSTRAINT fk_siswa FOREIGN KEY (`id_siswa`) REFERENCES siswa(`id_siswa`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
 
--- INSERT INTO absen (`id_absen`, `id_siswa`, `id_jadwal`, `waktu`, `jadwal`, `keterangan`) VALUES
--- ();
+CREATE TABLE `absen` (
+  `id_absen` int(10) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
+  `id_jadwal` int(11) NOT NULL,
+  `waktu` time DEFAULT current_timestamp(),
+  `tanggal` date DEFAULT current_timestamp(),
+  `keterangan` enum('H','S','I','A') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Table structure for table `user`
-CREATE TABLE user (
-    `id_user` INT PRIMARY KEY AUTO_INCREMENT,
-    `nama` VARCHAR(100) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `level` ENUM('admin', 'user') NOT NULL, -- Misalnya level dapat berupa 'admin' atau 'user'
-    `id` INT NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
 
--- Dumping data for table `user`
-INSERT INTO user(`id_user`, `nama`, `password`, `level`, `id`) VALUES
-(1, 'Shyy', 'abcdefg', 'admin', 2);
-
+--
 -- Table structure for table `guru`
-CREATE TABLE guru (
-    `id_guru` INT PRIMARY KEY AUTO_INCREMENT,
-    `nip` VARCHAR(20) NOT NULL UNIQUE,
-    `nama` VARCHAR(100) NOT NULL,
-    `jenis_kelamin` ENUM('L', 'P') NOT NULL,
-    `alamat` TEXT NOT NULL,
-    `password` VARCHAR(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
 
--- Dumping data for table `Guru`
-INSERT INTO guru(`id_guru`, `nip`, `nama`, `jenis_kelamin`, `alamat`, `password`) VALUES
-(1, '22334455', 'Fulan', 'L', '-', '12345');
+CREATE TABLE `guru` (
+  `id_guru` int(10) NOT NULL,
+  `nip` varchar(20) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `alamat` text NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data for table `guru`
+--
+
+INSERT INTO `guru` (`id_guru`, `nip`, `nama`, `jenis_kelamin`, `alamat`, `password`) VALUES
+(1, '123456', 'Asep', 'L', 'Jl.Kenangan', '$2y$10$6XFB9vQgOqGj/CRs.p/JE.bj1LxhlKnLqQNW9m8OpGwUPQddrmwle');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `hari`
-CREATE TABLE hari (
-    `id_hari` INT PRIMARY KEY AUTO_INCREMENT,
-    `hari` ENUM('Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
 
+CREATE TABLE `hari` (
+  `id_hari` int(11) NOT NULL,
+  `hari` enum('Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
 -- Dumping data for table `hari`
--- INSERT INTO hari (hari) VALUES 
--- ('Kamis');
+--
 
+INSERT INTO `hari` (`id_hari`, `hari`) VALUES
+(1, 'Senin'),
+(2, 'Selasa'),
+(3, 'Rabu'),
+(4, 'Kamis'),
+(5, 'Jumat'),
+(6, 'Sabtu'),
+(7, 'Minggu');
 
+-- --------------------------------------------------------
 
--- Table structure for table `kelas`
-CREATE TABLE kelas (
-    `id_kelas` INT PRIMARY KEY AUTO_INCREMENT,
-    `tingkatan` ENUM('10', '11', '12') NOT NULL,
-    `jurusan` ENUM('RPL', 'TKJ', 'AT', 'TEI', 'BR', 'TKI', 'AXIOO', 'ORACLE') NOT NULL,
-    `char` ENUM('A', 'B', 'C') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table `kelas`
--- INSERT INTO kelas (`id_kelas`, `tingkatan` ,`jurusan`, 'char') VALUES 
--- (1, '11','RPL', 'A');
-
--- Table structure for table `mapel`
-CREATE TABLE mapel (
-    `id_mapel` INT PRIMARY KEY AUTO_INCREMENT,
-    `nama_mapel` VARCHAR(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table `mapel`
--- INSERT INTO mapel (`id_mapel`, `nama_mapel`) VALUES
--- (1, 'Database');
-
+--
 -- Table structure for table `jadwal`
-CREATE TABLE jadwal (
-    `id_jadwal` INT PRIMARY KEY AUTO_INCREMENT,
-    `id_hari` INT NOT NULL,
-    `id_guru` INT NOT NULL,
-    `id_kelas` INT NOT NULL,
-    `id_mapel` INT NOT NULL,
-    `jam_mulai` TIME NOT NULL,
-    `jam_selesai` TIME NOT NULL,
-    `tanggal` DATE DEFAULT CURRENT_TIMESTAMP,
-    `aktif` INT NOT NULL,
-    CONSTRAINT fk_hari FOREIGN KEY (`id_hari`) REFERENCES hari(`id_hari`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    CONSTRAINT fk_guru FOREIGN KEY (`id_guru`) REFERENCES guru(`id_guru`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    CONSTRAINT fk_kelas FOREIGN KEY (`id_kelas`) REFERENCES kelas(`id_kelas`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    CONSTRAINT fk_mapel FOREIGN KEY (`id_mapel`) REFERENCES mapel(`id_mapel`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
 
--- Dumping data for table `jadwal`
--- INSERT INTO jadwal (`id_jadwal`, `id_hari`, `id_kelas`, `id_mapel`, `jam_mulai`, `jam_selesai`, `tanggal`, `aktif`) VALUES
--- (1, 1, 11, 2, '07:00:00', '09:00:00', '2024-10-24' ,0);
+CREATE TABLE `jadwal` (
+  `id_jadwal` int(11) NOT NULL,
+  `id_hari` int(11) NOT NULL,
+  `id_guru` int(11) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
+  `id_mapel` int(11) NOT NULL,
+  `jam_mulai` time NOT NULL,
+  `jam_selesai` time NOT NULL,
+  `tanggal` date NOT NULL,
+  `aktif` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(10) NOT NULL,
+  `tingkatan` enum('10','11','12') NOT NULL,
+  `jurusan` enum('RPL','TKJ','AT','TEI','BR','TKI','AXIOO','ORACLE','GAMELAB') NOT NULL,
+  `char` enum('A','B','C') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `tingkatan`, `jurusan`, `char`) VALUES
+(1, '10', 'RPL', 'A'),
+(2, '10', 'RPL', 'B'),
+(3, '10', 'TKJ', 'A'),
+(4, '10', 'TKJ', 'B'),
+(5, '10', 'AT', 'A'),
+(6, '10', 'AT', 'B'),
+(7, '10', 'TEI', 'A'),
+(8, '10', 'TEI', 'B'),
+(9, '10', 'BR', 'A'),
+(10, '10', 'BR', 'B'),
+(11, '10', 'TKI', 'A'),
+(12, '10', 'TKI', 'B'),
+(13, '10', 'TKI', 'C'),
+(14, '10', 'AXIOO', 'A'),
+(15, '10', 'GAMELAB', 'A'),
+(16, '11', 'RPL', 'A'),
+(17, '11', 'RPL', 'B'),
+(18, '11', 'TKJ', 'A'),
+(19, '11', 'TKJ', 'B'),
+(20, '11', 'AT', 'A'),
+(21, '11', 'AT', 'B'),
+(22, '11', 'AT', 'C'),
+(23, '11', 'TEI', 'A'),
+(24, '11', 'BR', 'A'),
+(25, '11', 'BR', 'B'),
+(26, '11', 'TKI', 'A'),
+(27, '11', 'TKI', 'B'),
+(28, '11', 'AXIOO', 'A'),
+(29, '11', 'ORACLE', 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mapel`
+--
+
+CREATE TABLE `mapel` (
+  `id_mapel` int(11) NOT NULL,
+  `nama_mapel` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `mapel`
+--
+
+INSERT INTO `mapel` (`id_mapel`, `nama_mapel`) VALUES
+(1, 'Database'),
+(2, 'PBO'),
+(3, 'Web Development'),
+(4, 'Mobile Development'),
+(5, 'Game Development'),
+(6, 'P5'),
+(7, 'IPAS'),
+(8, 'PKK'),
+(9, 'Matematika'),
+(10, 'PAI'),
+(11, 'Bahasa Indonesia'),
+(12, 'Bahasa Sunda'),
+(13, 'Bahasa Inggris'),
+(14, 'PENJAS'),
+(15, 'Sejarah'),
+(16, 'Seni Budaya'),
+(17, 'PKN'),
+(18, 'BK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id_permission` int(11) NOT NULL,
+  `action` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id_permission`, `action`) VALUES
+(3, 'delete_user'),
+(2, 'edit_profile'),
+(1, 'view_dashboard'),
+(4, 'view_users');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `role_name` enum('admin','user') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`role_name`) VALUES
+('admin'),
+('user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_permissions`
+--
+
+CREATE TABLE `role_permissions` (
+  `role_name` enum('admin','user') NOT NULL,
+  `id_permission` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role_permissions`
+--
+
+INSERT INTO `role_permissions` (`role_name`, `id_permission`) VALUES
+('admin', 1),
+('admin', 2),
+('admin', 3),
+('admin', 4),
+('user', 1),
+('user', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id_siswa` int(10) NOT NULL,
+  `nisn` varchar(10) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `alamat` text NOT NULL,
+  `id_kelas` int(5) NOT NULL,
+  `no_telepon` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id_siswa`, `nisn`, `nama`, `jenis_kelamin`, `alamat`, `id_kelas`, `no_telepon`, `password`) VALUES
+(33, '0074095293', 'Syahrul Hidayatulloh', 'P', 'Kp Cempaka RT01/03', 16, '083820103522', '$2y$10$3dNAagmT.OPQritIApgoIOdLV5Bx/0/4u3m/9qBMFNeF1Pjkj7DSm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(10) NOT NULL,
+  `id_user_guru` int(11) DEFAULT NULL,
+  `id_user_siswa` int(11) DEFAULT NULL,
+  `user_type` enum('siswa','guru') NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','user') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `id_user_guru`, `id_user_siswa`, `user_type`, `username`, `password`, `role`) VALUES
+(1, NULL, 33, 'siswa', '0074095293', '$2y$10$3dNAagmT.OPQritIApgoIOdLV5Bx/0/4u3m/9qBMFNeF1Pjkj7DSm', 'user'),
+(2, 1, NULL, 'guru', '123456', '$2y$10$6XFB9vQgOqGj/CRs.p/JE.bj1LxhlKnLqQNW9m8OpGwUPQddrmwle', 'admin');
+
+--
+-- Triggers `user`
+--
+DELIMITER $$
+CREATE TRIGGER `before_insert_username` BEFORE INSERT ON `user` FOR EACH ROW BEGIN
+    -- Memeriksa tipe pengguna baru
+    IF NEW.user_type = 'siswa' THEN
+        -- Ambil nisn dan password dari tabel siswa
+        SET NEW.username = (SELECT nisn FROM siswa WHERE siswa.id_siswa = NEW.id_user_siswa);
+        SET NEW.password = (SELECT password FROM siswa WHERE siswa.id_siswa = NEW.id_user_siswa);
+        SET NEW.id_user_guru = NULL; -- Mengatur id_user_guru menjadi NULL jika pengguna adalah siswa
+    ELSEIF NEW.user_type = 'guru' THEN
+        -- Ambil nip dan password dari tabel guru
+        SET NEW.username = (SELECT nip FROM guru WHERE guru.id_guru = NEW.id_user_guru);
+        SET NEW.password = (SELECT password FROM guru WHERE guru.id_guru = NEW.id_user_guru);
+        SET NEW.id_user_siswa = NULL; -- Mengatur id_user_siswa menjadi NULL jika pengguna adalah guru
+    END IF;
+
+    -- Tambahkan pemeriksaan untuk mencegah NULL
+    IF NEW.username IS NULL THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Username tidak ditemukan!';
+    END IF;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `absen`
+--
+ALTER TABLE `absen`
+  ADD PRIMARY KEY (`id_absen`),
+  ADD KEY `fk_siswa` (`id_siswa`),
+  ADD KEY `id_jadwal` (`id_jadwal`);
+
+--
+-- Indexes for table `guru`
+--
+ALTER TABLE `guru`
+  ADD PRIMARY KEY (`id_guru`),
+  ADD UNIQUE KEY `nip` (`nip`),
+  ADD KEY `fk_nama` (`nama`),
+  ADD KEY `fk_password` (`password`);
+
+--
+-- Indexes for table `hari`
+--
+ALTER TABLE `hari`
+  ADD PRIMARY KEY (`id_hari`);
+
+--
+-- Indexes for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `fk_hari` (`id_hari`),
+  ADD KEY `fk_guru` (`id_guru`),
+  ADD KEY `fk_kelas` (`id_kelas`),
+  ADD KEY `fk_mapel` (`id_mapel`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `mapel`
+--
+ALTER TABLE `mapel`
+  ADD PRIMARY KEY (`id_mapel`);
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id_permission`),
+  ADD UNIQUE KEY `action` (`action`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`role_name`);
+
+--
+-- Indexes for table `role_permissions`
+--
+ALTER TABLE `role_permissions`
+  ADD PRIMARY KEY (`role_name`,`id_permission`),
+  ADD KEY `fk_permission` (`id_permission`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id_siswa`),
+  ADD UNIQUE KEY `nisn` (`nisn`),
+  ADD KEY `fk_nama` (`nama`),
+  ADD KEY `fk_password` (`password`),
+  ADD KEY `fk_kelas` (`id_kelas`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `fk_password` (`password`),
+  ADD KEY `fk_username` (`username`) USING BTREE,
+  ADD KEY `role` (`role`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
 --
 -- AUTO_INCREMENT for table `absen`
 --
 ALTER TABLE `absen`
   MODIFY `id_absen` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `guru`
 --
 ALTER TABLE `guru`
   MODIFY `id_guru` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
 --
 -- AUTO_INCREMENT for table `hari`
 --
 ALTER TABLE `hari`
-  MODIFY `id_hari` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_hari` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT for table `jadwal`
 --
 ALTER TABLE `jadwal`
   MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_kelas` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
 --
--- AUTO_INCREMENT for table `mata_pelajaran`
+-- AUTO_INCREMENT for table `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id_permission` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
   MODIFY `id_siswa` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `absen`
+--
+ALTER TABLE `absen`
+  ADD CONSTRAINT `fk_jadwal` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal` (`id_jadwal`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_siswa` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id_siswa`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD CONSTRAINT `fk_guru` FOREIGN KEY (`id_guru`) REFERENCES `guru` (`id_guru`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_hari` FOREIGN KEY (`id_hari`) REFERENCES `hari` (`id_hari`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_kelas` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_mapel` FOREIGN KEY (`id_mapel`) REFERENCES `mapel` (`id_mapel`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `role_permissions`
+--
+ALTER TABLE `role_permissions`
+  ADD CONSTRAINT `fk_permission` FOREIGN KEY (`id_permission`) REFERENCES `permissions` (`id_permission`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_role_name` FOREIGN KEY (`role_name`) REFERENCES `roles` (`role_name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `fk_kelas_siswa` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_role` FOREIGN KEY (`role`) REFERENCES `roles` (`role_name`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
