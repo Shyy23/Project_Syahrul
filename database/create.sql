103f0e1e39c2feedb078dcb131d3a0df92785bfa
Shyy23 <syahrulhidayatulloh14@gmail.com>
2024-10-31 07:55:53 +0700

    DatabaseNew

--- database/create.sql
+++ database/create.sql
@@ -102,24 +102,7 @@ INSERT INTO `siswa` (`id_siswa`, `nisn`, `nama`, `jenis_kelamin`, `alamat`, `kel
 ALTER TABLE `siswa`
 ADD PRIMARY KEY (`id_siswa`);
 
--- CRUD -> R -> READ
--- Mengambil Semua Data
-SELECT * FROM siswa;
 
--- Mengambil dengan kriteria tertentu
-SELECT * FROM siswa WHERE kelas = '11';
-
--- Mengambil data dengan filter beberapa kolom
-SELECT nisn, nama, kelas FROM siswa;
-
--- Mengambil data dengan sorting
-SELECT * FROM siswa ORDER BY nama DESC;
-
--- Mengambil data dengan 
-SELECT * FROM siswa WHERE nama LIKE 'syahrul';
-
--- Mengambil data dengan LIMIT
-SELECT * FROM siswa LIMIT 5;
 
 -- CRUD -> U -> UPDATE
 -- update with where
